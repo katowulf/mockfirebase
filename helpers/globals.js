59bb8f3edc252ff1d3baf9cd58aad65c3d56663b
Brian Soumakian <bsoumakian@gmail.com>
2016-08-31 04:22:39 -0700

    add basic database support for firebase sdk 3.x

--- helpers/globals.js
+++ helpers/globals.js
@@ -7,14 +7,28 @@
     var originals = false;
     window.MockFirebase.override = function () {
       originals = {
+        firebase3: window.firebase,
         firebase: window.Firebase,
         login: window.FirebaseSimpleLogin
+      };
+      window.firebase = {
+        database: function() {
+          return {
+            ref: function(path) {
+              return new window.mockfirebase.MockFirebase(path);
+            },
+            refFromURL: function(url) {
+              return new window.mockfirebase.MockFirebase(url);
+            }
+          };
+        }
       };
       window.Firebase = window.mockfirebase.MockFirebase;
       window.FirebaseSimpleLogin = window.mockfirebase.MockFirebaseSimpleLogin;
     };
     window.MockFirebase.restore = function () {
       if (!originals) return;
+      window.firebase = originals.firebase3;
       window.Firebase = originals.firebase;
       window.FirebaseSimpleLogin = originals.login;
     };
