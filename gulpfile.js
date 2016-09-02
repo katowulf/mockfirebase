80b48c8b52b2ecd491ad9f71d928c6e741f728c4
Brian Soumakian <bsoumakian@gmail.com>
2016-09-01 21:08:00 -0700

    use gulp magic to fix build
    removes need for stream-to-promise dependency

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ var source          = require('vinyl-source-stream');
 var buffer          = require('vinyl-buffer');
 var fs              = require('fs');
 var argv            = require('yargs').argv;
-var streamToPromise = require('stream-to-promise');
 var path            = require('path');
 var os              = require('os');
 
@@ -35,6 +34,16 @@ function bundle () {
     }))
     .pipe(plugins.footer(fs.readFileSync('./helpers/globals.js')));
 }
+
+var bundlePath;
+gulp.task('bundle-smoke', function () {
+  var name = Date.now() + '-mockfirebase.js';
+  var dir = os.tmpdir();
+  bundlePath = path.join(dir, name);
+  return bundle()
+    .pipe(plugins.rename(name))
+    .pipe(gulp.dest(dir));
+});
 
 gulp.task('bundle', function () {
   return bundle().pipe(gulp.dest('./browser'));
@@ -76,28 +85,20 @@ gulp.task('karma', function () {
   });
 });
 
-gulp.task('smoke', function () {
-  var name = Date.now() + '-mockfirebase.js';
-  var dir = os.tmpdir();
-  var bundlePath = path.join(dir, name);
-  return streamToPromise(bundle()
-    .pipe(plugins.rename(name))
-    .pipe(gulp.dest(dir)))
-    .then(function () {
-      return require('karma-as-promised').server.start({
-        frameworks: ['mocha', 'chai'],
-        browsers: ['PhantomJS'],
-        client: {
-          args: ['--grep', argv.grep]
-        },
-        files: [
-          bundlePath,
-          'test/smoke/globals.js'
-        ],
-        autoWatch: false,
-        singleRun: true
-      });
-    });
+gulp.task('smoke', ['bundle-smoke'], function () {
+  return require('karma-as-promised').server.start({
+    frameworks: ['mocha', 'chai'],
+    browsers: ['PhantomJS'],
+    client: {
+      args: ['--grep', argv.grep]
+    },
+    files: [
+      bundlePath,
+      'test/smoke/globals.js'
+    ],
+    autoWatch: false,
+    singleRun: true
+  });
 });
 
 gulp.task('lint', function () {
